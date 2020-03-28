7ad01af16c59c1874e0a774dff33371ca588d2d1
Manuel Alvarez <manuel@pythiac.com>
2020-03-28 16:45:12 +0100

    Fix minor issues on ftp code, improve data formatting performance.

--- tasks/noaa/ftp.py
+++ tasks/noaa/ftp.py
@@ -1,9 +1,11 @@
-'''Extract and prepare meteorological data from https://www.ncdc.noaa.gov/
+"""Extract and prepare meteorological data from https://www.ncdc.noaa.gov/
 the National Centers for Environmental Information
-'''
+"""
 
+from datetime import datetime
 import logging
 import os
+import re
 import tarfile as tar
 from ftplib import FTP
 
@@ -22,9 +24,17 @@ NOAA_FTP_FILES = {
     'all_daily_data.tar.gz': 'RETR ghcnd_all.tar.gz'
 }
 
+INDEX_COLUMNS = [
+    'ID',
+    'YEAR',
+    'MONTH',
+    'ELEMENT'
+]
+REGEX = re.compile('^(\w*?)(\d+)$')
 
-def download_noaa_files(large_files=True, skip_downloaded=True):
-    '''Download files from the NOAA FTP server.
+
+def download_noaa_files(large_files=True, skip_downloaded=False):
+    """Download files from the NOAA FTP server.
     
     Arguments:
         large_files(bool):
@@ -35,14 +45,14 @@ def download_noaa_files(large_files=True, skip_downloaded=True):
 
     Returns:
         None. The files will be downloaded on DOWNLOADED_DIRECTORY.
-    '''
+    """
     logging.info('Connecting to NOAA FTP server.')
     ftp = FTP('ftp.ncdc.noaa.gov')
-    print(ftp.login())
+    ftp.login()
 
     ftp.cwd('/pub/data/ghcn/daily/')
 
-    for filename, action in NOAA_FTP_FILES:
+    for filename, action in NOAA_FTP_FILES.items():
         logging.debug('Downloading %s', filename)
         if filename.endswith('tar.gz'):
             if not large_files:
@@ -50,10 +60,15 @@ def download_noaa_files(large_files=True, skip_downloaded=True):
 
             logging.debug('This is file is more than 3Gb+, it may take a long time.')
         path = os.path.join(DATA_DIRECTORY, filename)
+
+        if not os.path.exists(path) and not os.path.isdir(DATA_DIRECTORY):
+            os.mkdir(DATA_DIRECTORY)
+
         if skip_downloaded:
             server_file_name = action.split(' ')[1]
             if os.path.exists(path) and ftp.size(server_file_name) == os.stat(path).st_size:
                 continue
+
         with open(path, 'wb') as fp:
             ftp.retrbinary(action, fp.write)
 
@@ -64,44 +79,47 @@ def download_noaa_files(large_files=True, skip_downloaded=True):
     logging.debug('Done!')
 
 
-def reshape_daily_data(month_data):
-    '''Reshape a row of monthly data into a dataframe with date as a column.'''
-    month_data = month_data.iloc[0]
-
-    station_id = month_data['ID']
-    year = month_data['YEAR']
-    month = month_data['MONTH']
-    element = month_data['ELEMENT']
-
-    data = [{
-        'ID': station_id,
-        'DATE': f'{day}-{month}-{year}',
-        'ELEMENT': element,
-        'VALUE': month_data[f'VALUE{day}'],
-        'MEASURMENT_FLAG': month_data[f'MFLAG{day}'],
-        'QUALITY_FLAG': month_data[f'QFLAG{day}'],
-        'SOURCE_FLAG': month_data[f'SFLAG{day}'],
-    } for day in range(1, 32)]
-
-    return pd.DataFrame(data)
+def to_date(row):
+    try:
+        return datetime(row['YEAR'], row['MONTH'], row['DAY'])
+    except ValueError:
+        return None
 
 
-def time_series_of_station(station_row):
-    station_name = station_row.iloc[0]['ID']
-    df = load_dataset(station_name)
-    # Select data starting from November 2019
-    df = df[(df.YEAR >= 2020) | ((df.YEAR == 2019) & (df.MONTH >= 11))]
+def load_and_filter_dataset(dataset_name):
+    dataset = load_dataset(dataset_name)
+    return dataset[(dataset.YEAR >= 2020) | ((dataset.YEAR == 2019) & (dataset.MONTH >= 11))]
 
-    if df.empty:
-        return
 
-    # Put data in a time series
-    df = df.groupby(['ID', 'YEAR', 'MONTH', 'ELEMENT']).apply(reshape_daily_data)
-    return df.reset_index(drop=True)
+def load_stations_data(station_ids):
+    stations_data = pd.concat([
+        load_and_filter_dataset(station_id)
+        for station_id in station_ids
+    ]).set_index(INDEX_COLUMNS)
+
+    stacked = stations_data.stack().reset_index(level=-1)
+    stacked.columns = ['name', 'value']
+
+    column_day = stacked.name.str.extract(REGEX)
+    stacked['column'] = column_day[0]
+    stacked['DAY'] = column_day[1].astype(int)
+    del stacked['name']
+
+    values = stacked.set_index(['DAY', 'column'], append=True)
+
+    unstacked = values.unstack(level=-1)
+    unstacked.columns = unstacked.columns.droplevel(0)
+    unstacked.reset_index(inplace=True)
+    unstacked.columns = list(unstacked.columns)
+
+    unstacked['DATE'] = unstacked.apply(to_date, axis=1)
+    unstacked.drop(labels=['YEAR', 'MONTH', 'DAY'], axis=1, inplace=True)
+
+    return unstacked.dropna(subset=['DATE'])
 
 
 def process_noaa_data(countries):
-    '''Returns a dataset for the given countries '''
+    """Returns a dataset for the given countries."""
 
     df_stations = load_dataset('stations')
     df_countries = load_dataset('countries')
@@ -112,8 +130,6 @@ def process_noaa_data(countries):
 
     df_stations = df_stations[df_stations['COUNTRY_CODE'].isin(countries)]
 
-    df_daily_information = df_stations[['ID']].groupby('ID').apply(time_series_of_station)
-    df_daily_information.reset_index(drop=True, inplace=True)
+    df_daily_information = load_stations_data(df_stations['ID'])
 
     return df_daily_information.merge(df_stations, how='left', on=['ID'])
-
